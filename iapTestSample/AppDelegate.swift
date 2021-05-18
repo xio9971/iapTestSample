7ac93332505d2bba326ffbb5bae82b47b2f48cda
민트팟 <mintpot@minteupas-ui-MacBookPro.local>
2021-05-18 13:42:51 +0900

    인앱결제테스트

---
+++ iapTestSample/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  iapTestSample
+//
+//  Created by 정창규 on 2021/05/17.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
